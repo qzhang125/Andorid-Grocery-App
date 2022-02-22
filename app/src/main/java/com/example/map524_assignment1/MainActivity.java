b9b4f346803fd2fb24a3f5a9cd4ef26e34d8928e
qiuyu <qzhang125@myseneca.ca>
2022-02-22 00:48:09 -0500

    Done

--- app/src/main/java/com/example/map524_assignment1/MainActivity.java
+++ app/src/main/java/com/example/map524_assignment1/MainActivity.java
@@ -32,17 +32,17 @@ public class MainActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //if(emailAddress.getText().toString().equals("admin@example.com") && password.getText().toString().equals("1234")) {
+                if(emailAddress.getText().toString().equals("admin@example.com") && password.getText().toString().equals("1234")) {
                     Intent loginIntent = new Intent(MainActivity.this, StorePageActivity.class);
                     String tempUserName = username.getText().toString();
                     loginIntent.putExtra("name",tempUserName);
                     startActivity(loginIntent);
                     errorMsg.setText("");
-                //}else{
-                    ///errorMsg.setText("Invalid email or password, please try again");
-                    //InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
-                    //imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
-                //}
+                }else{
+                    errorMsg.setText("Invalid email or password, please try again");
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+                }
             }
         });
 
--- app/src/main/java/com/example/map524_assignment1/RecyclerAdapter.java
+++ app/src/main/java/com/example/map524_assignment1/RecyclerAdapter.java
@@ -1,11 +1,14 @@
 package com.example.map524_assignment1;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -39,7 +42,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Recycl
         return storelist.length;
     }
 
-    public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView storeName;
         TextView storeAddress;
         ImageView storeImage;
@@ -48,9 +51,103 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Recycl
             super(itemView);
 
             storeName = itemView.findViewById(R.id.textView10);
+            storeName.setOnClickListener(this);
             storeAddress = itemView.findViewById(R.id.textView11);
+            storeAddress.setOnClickListener(this);
             storeImage = itemView.findViewById(R.id.imageView5);
+            storeImage.setOnClickListener(this);
             checkImage = itemView.findViewById(R.id.imageView6);
+            checkImage.setOnClickListener(this);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.textView10:
+                    showNameDialog();
+                    break;
+                case R.id.textView11:
+                    showAddressDialog();
+                    break;
+                case R.id.imageView5:
+                    showIconDialog();
+                    break;
+                case R.id.imageView6:
+                    showStatusDialog();
+                    break;
+                default:
+                    showTheRow();
+                    break;
+            }
+        }
+
+        void showNameDialog() {
+            AlertDialog.Builder nameAlert= new AlertDialog.Builder(context, AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
+            nameAlert.setTitle("Click information");
+            nameAlert.setIcon(R.drawable.ic_baseline_info_24);
+            nameAlert.setMessage("You clicked the store name");
+            nameAlert.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            nameAlert.create().show();
+        }
+        void showAddressDialog(){
+            AlertDialog.Builder addressAlert= new AlertDialog.Builder(context, AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
+            addressAlert.setTitle("Click information");
+            addressAlert.setIcon(R.drawable.ic_baseline_info_24);
+            addressAlert.setMessage("You clicked the store address");
+            addressAlert.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            addressAlert.create().show();
+        }
+        void showIconDialog(){
+            AlertDialog.Builder iconAlert= new AlertDialog.Builder(context, AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
+            iconAlert.setTitle("Click information");
+            iconAlert.setIcon(R.drawable.ic_baseline_info_24);
+            iconAlert.setMessage("You clicked the store logo");
+            iconAlert.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            iconAlert.create().show();
+        }
+        void showStatusDialog(){
+            AlertDialog.Builder statusAlert= new AlertDialog.Builder(context, AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
+            statusAlert.setTitle("Click information");
+            statusAlert.setIcon(R.drawable.ic_baseline_info_24);
+            statusAlert.setMessage("You clicked the status");
+            statusAlert.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            statusAlert.create().show();
+        }
+        void showTheRow(){
+            int index = getAdapterPosition();
+            AlertDialog.Builder rowAlert= new AlertDialog.Builder(context, AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
+            rowAlert.setTitle("Click information");
+            rowAlert.setIcon(R.drawable.ic_baseline_info_24);
+            rowAlert.setMessage("You clicked the " + storelist[index].storeName + " row.");
+            rowAlert.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            rowAlert.create().show();
         }
     }
 }
